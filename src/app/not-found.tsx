655aa9522f6a5b5b52fb593bbcc080b3ba654fb8
devjpedro <joaopedross.ti9@gmail.com>
2024-09-26 20:25:25 -0300

    finished not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <section className="container">
+      <h1 className="title">Página não encontrada</h1>
+      <Link
+        href="/"
+        className="button"
+        style={{ marginBottom: '1rem', display: 'inline-block' }}
+      >
+        Volte para a Home.
+      </Link>
+    </section>
+  );
+}
