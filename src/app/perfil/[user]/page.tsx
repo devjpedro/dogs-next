95369e8da9269f4eccf901f71002ac3307c0e678
devjpedro <joaopedross.ti9@gmail.com>
2024-09-27 20:17:42 -0300

    finished user perfil page

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,13 +1,20 @@
+import getPhotos from '@/actions/get-photos';
+import Feed from '@/components/feed';
 import React from 'react';
 
-export default function PerfilUserPage({
+export default async function PerfilUserPage({
   params,
 }: {
   params: { user: string };
 }) {
+  const { data } = await getPhotos({ user: params.user });
+
+  if (!data) return null;
+
   return (
-    <div>
-      <h1>Usuário: {params.user}</h1>
-    </div>
+    <section className="container mainSection">
+      <h1 className="title">{params.user}</h1>
+      <Feed photos={data} user={params.user} />
+    </section>
   );
 }
